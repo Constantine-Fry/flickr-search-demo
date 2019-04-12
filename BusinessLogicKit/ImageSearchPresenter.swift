9beb17fe4ddb47783d9dd6aaf44d43e1947a8ec8
Constantine Fry <constantine.fry@gmail.com>
2019-04-12 21:51:18 +0200

    Move protocols.

--- BusinessLogicKit/ImageSearchPresenter.swift
+++ BusinessLogicKit/ImageSearchPresenter.swift
@@ -3,32 +3,6 @@
 //  Copyright © 2019 lololo. All rights reserved.
 //
 
-public protocol ImageSearchViewing: class {
-    func update(_ state: ImageViewState)
-}
-
-public protocol ImageSearchUseCase {
-    func search(query: SearchQuery, completion: @escaping (Result<Page, Error>) -> Void)
-}
-
-public protocol ImagesUseCase {
-    func cachedImage(for photo: Photo) -> Image?
-    func loadImage(for photo: Photo, completion: @escaping (Image?) -> Void)
-    func stopLoadImage(for photo: Photo)
-}
-
-public protocol ImageSearchPresenting {
-    func search(term: String)
-    func loadMore(for item: ImageSearchViewItem)
-}
-
-public protocol ImageLoadPresenting {
-    func cachedImage(for photo: Photo) -> CGImage?
-    func loadImage(for photo: Photo, completion: @escaping (CGImage?) -> Void)
-    func stopLoadImage(for photo: Photo)
-}
-
-
 public final class ImageSearchPresenter {
 
     public weak var view: ImageSearchViewing?
---
+++ BusinessLogicKit/ImageSearchPresenterProtocols.swift
@@ -0,0 +1,29 @@
+//
+//  Created by Constantine Fry on 12.04.19.
+//  Copyright © 2019 lololo. All rights reserved.
+//
+
+public protocol ImageSearchViewing: class {
+    func update(_ state: ImageViewState)
+}
+
+public protocol ImageSearchUseCase {
+    func search(query: SearchQuery, completion: @escaping (Result<Page, Error>) -> Void)
+}
+
+public protocol ImagesUseCase {
+    func cachedImage(for photo: Photo) -> Image?
+    func loadImage(for photo: Photo, completion: @escaping (Image?) -> Void)
+    func stopLoadImage(for photo: Photo)
+}
+
+public protocol ImageSearchPresenting {
+    func search(term: String)
+    func loadMore(for item: ImageSearchViewItem)
+}
+
+public protocol ImageLoadPresenting {
+    func cachedImage(for photo: Photo) -> CGImage?
+    func loadImage(for photo: Photo, completion: @escaping (CGImage?) -> Void)
+    func stopLoadImage(for photo: Photo)
+}
