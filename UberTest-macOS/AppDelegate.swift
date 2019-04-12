029789ab3653c90445764cf87aca52ea2d419f1b
Constantine Fry <constantine.fry@gmail.com>
2019-04-12 22:50:55 +0200

    Add macOS target.

---
+++ UberTest-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UberTest-macOS
+//
+//  Created by Constantine Fry on 12.04.19.
+//  Copyright © 2019 lololo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UberTest-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UberTest-macOS
+//
+//  Created by Constantine Fry on 12.04.19.
+//  Copyright © 2019 lololo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
